276f9750cbcde0a0c68fad83261472ba83b8f513
Joseph Morris <jrm.softworks@gmail.com>
2017-11-30 13:09:05 -0500

    Completed the Products section prototype. Implemented JS swiping/slider functions as well. Will work on some more styling issues and then start tablet implementation.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,98 @@
 /**
  * Created by Joseph on 11/23/2017.
  */
+
+var RIGHT = "right",
+    LEFT = "left",
+    LEFT_ARROW = $("#productLeft"),
+    RIGHT_ARROW = $("#productRight"),
+    SLIDER = $('#productSlider');
+
+setLeftArrowActive(false);
+
+RIGHT_ARROW.click(function (e) {
+    nextItem()
+});
+
+LEFT_ARROW.click(function (e) {
+    previousItem();
+});
+
+SLIDER.on("swipeleft", function(e) {
+    nextItem();
+});
+
+SLIDER.on("swiperight", function(e) {
+    previousItem();
+});
+
+
+
+
+function nextItem() {
+    var slideAmount = $('body').width() - 35;
+
+    if (RIGHT_ARROW.hasClass("disabled")) {
+        return false;
+    }
+
+    if (!checkSlideBoundaries(SLIDER, RIGHT, slideAmount * 2)) {
+        setRightArrowActive(false);
+    }
+
+    setLeftArrowActive(true);
+
+    slideLeft(SLIDER, slideAmount);
+}
+
+function previousItem() {
+    var slideAmount = $('body').width() - 35;
+
+    if (LEFT_ARROW.hasClass("disabled")) {
+        return false;
+    }
+
+    if (!checkSlideBoundaries(SLIDER, LEFT, slideAmount)) {
+        setLeftArrowActive(false);
+    }
+
+    setRightArrowActive(true);
+
+    slideRight(SLIDER, slideAmount);
+}
+
+function slideLeft(el, amount) {
+    el.css("right", "+=" + amount.toString());
+}
+
+function slideRight(el, amount) {
+    el.css("right", "-=" + amount.toString());
+}
+
+// Returns false if sliding would move the element outside of its boundaries
+function checkSlideBoundaries(el, slideDirection, slideAmount) {
+    var futurePos = parseFloat(el.css("right")),
+        boundary = (slideDirection == "right") ? el[0].scrollWidth : 0;
+
+    slideAmount *= (slideDirection == "left") ? -1 : 1;
+
+    futurePos += slideAmount;
+
+    return (slideDirection == "right") ? futurePos < boundary : futurePos > 0;
+}
+
+function setLeftArrowActive(active) {
+    setActive(LEFT_ARROW, active);
+}
+
+function setRightArrowActive(active) {
+    setActive(RIGHT_ARROW, active);
+}
+
+function setActive(el, active) {
+    var fn = (active) ? el.removeClass : el.addClass;
+    fn.call(el, "disabled");
+}
+
+
+
