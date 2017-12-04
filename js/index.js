6acb9b498e32be3242488dbda19779bd5a969d4f
Joseph Morris <jrm.softworks@gmail.com>
2017-12-04 01:09:57 -0500

    Finished initial JS for shopping cart. JS and CSS need some cleaning before tablet implementation can be started.

--- js/index.js
+++ js/index.js
@@ -7,7 +7,9 @@ var RIGHT = "right",
     LEFT_ARROW = $("#productLeft"),
     RIGHT_ARROW = $("#productRight"),
     SLIDER = $('#productSlider'),
-    MENU = $('#mobileNav');
+    MENU = $('#mobileNav'),
+    CART = $('#shoppingCartModal'),
+    transitioning = false;
 
 setLeftArrowActive(false);
 
@@ -60,11 +62,32 @@ function previousItem() {
 }
 
 function slideLeft(el, amount) {
+    if (transitioning) {
+        return false;
+    }
+
+    transitioning = true;
+    el.on("transitionend", transitionDone);
     el.css("right", "+=" + amount.toString());
 }
 
 function slideRight(el, amount) {
-    el.css("right", "-=" + amount.toString());
+    if (transitioning) {
+        return false;
+    }
+
+    transitioning = true;
+    el.on("transitionend", transitionDone);
+
+    if (el && +el.css("right").replace("px","") - amount < 0) {
+        el.css("right", "0");
+    } else {
+        el.css("right", "-=" + amount.toString());
+    }
+}
+
+function transitionDone() {
+    transitioning = false;
 }
 
 // Returns false if sliding would move the element outside of its boundaries
@@ -97,6 +120,113 @@ function hideMenu() {
     return true;
 }
 
+function containEmptyObjs(jqObjs) {
+
+    if (jqObjs.length == 0) {
+        return true;
+    }
+
+    jqObjs.each(function () {
+       if (this.length == 0) {
+           return true;
+       }
+    });
+
+    return false;
+}
+
+
+function addToCart(el) {
+    var item = $(el).closest('.product'),
+        title = item.find('.product-title'),
+        price = item.find('.price'),
+        img = item.find('.product-img'),
+        template = $('#scEntryTemplate'),
+        addedMsg = $('#addedToCartModal'),
+        cartTotal,
+        existingEntry,
+        entry;
+
+    if (containEmptyObjs($([
+            item,
+            title,
+            price,
+            img,
+            template,
+            CART
+        ]))) {
+        return false;
+    }
+
+    // Now that we know elements exist, we can set some more variables.
+    cartTotal = +CART.attr("data-cart-count");
+    existingEntry = $('.item-title:contains(' + title.text() + ')');
+
+    if (!containEmptyObjs(existingEntry)) {
+        // We already have this in the cart. Just update the quantity.
+        var quantity = existingEntry.closest('.cart-entry').find('.item-quantity');
+        quantity.text(+quantity.text() + 1);
+    } else {
+
+
+        entry = $([]).add(template[0].innerHTML);
+        entry.find('.item-title').text(title.text());
+        entry.find('.item-price').text(price.text());
+        entry.find('.item-quantity').text("1");
+        entry.find('.item-img').attr("src", img.attr("src"))
+                               .attr("alt", img.attr("alt"));
+
+        CART.find('.modal-body').append(entry);
+    }
+
+    updateCartSubtotal(parseFloat(price.text().replace("$","")));
+    updateCartCount(cartTotal + 1);
+    addedMsg.modal('show');
+}
+
+function removeFromCart(el) {
+    var item = $(el).closest(".cart-entry"),
+        quantity = item.find('.item-quantity'),
+        quantityVal = +quantity.text(),
+        price = item.find('.item-price'),
+        cartTotal;
+
+    if (containEmptyObjs($([
+            item,
+            quantity,
+            price,
+            CART
+        ]))) {
+            return false;
+    }
+
+    cartTotal = +CART.attr("data-cart-count");
+    price = parseFloat(price.text().replace("$",""));
+
+    if (quantityVal !== 1) {
+        quantity.text(quantityVal - 1);
+    } else {
+        item.remove();
+    }
+
+    updateCartSubtotal(price * -1);
+    updateCartCount(cartTotal - 1);
+}
+
+function updateCartCount(cartCount) {
+    CART.attr('data-cart-count', cartCount);
+    $("#scNumItems").text(cartCount);
+    $(".cart-count").text("(" + cartCount + ")");
+}
+
+function updateCartSubtotal(subtotal) {
+    var cartSubtotal = $('#subtotal');
+
+    if (!containEmptyObjs(cartSubtotal)) {
+        var newTotal = +cartSubtotal.text() + subtotal;
+        cartSubtotal.text(newTotal.toFixed(2));
+    }
+}
 
 
 
